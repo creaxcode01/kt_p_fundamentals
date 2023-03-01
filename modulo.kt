75586a38f896e11545dff5fefb61b30bd2c7db66
creaxcode01l <creaxcode01@outlook.fr>
2023-03-01 18:46:40 +0100

    ajout du fichier modulo.kt

---
+++ modulo.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+
+  println("La somme finale est de $finalAmount euros")
+  println(7 % 2) // 7 en deux parties fait 3 et 3 et il reste un 
+  println(10 % 3) // si on divise 10 en 3 parties quelle chiffre (entier) reste-t-il ?
+  println(9 % 3)
+  println(15 % 4) // si on divise 15 en quatres parties = 3 + 3 + 3 + 3 = 12, il reste donc 3 entier
+}
